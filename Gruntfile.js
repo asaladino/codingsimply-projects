6aadcf7ee89a233763b51803ce940352772189b6
Adam Saladino <adam@codingsimply.com>
2017-08-03 08:52:32 -0400

    Added grunt build.

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            build: ['build/codingsimply-projects']
+        },
+        copy: {
+            libs: {
+                files: []
+            },
+            build: {
+                files: [
+                    {
+                        src: [
+                            'src/**',
+                            '*.php',
+                            'README.md',
+                            'LICENSE'
+                        ],
+                        dest: 'build/codingsimply-projects/'
+                    }
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('build', ['clean:build', 'copy:build']);
+
+    grunt.registerTask('default', ['build']);
+};
