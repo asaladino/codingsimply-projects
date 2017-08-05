747cf7ae4c35ee25d0ed2114aec233e5d73cb9ae
Adam Saladino <adam@codingsimply.com>
2017-08-05 16:10:32 -0400

    Projects repository actually returns an array of project objects.

--- src/Core/Repository/ProjectsRepository.php
+++ src/Core/Repository/ProjectsRepository.php
@@ -3,11 +3,19 @@
 namespace src\Core\Repository;
 
 use CodingSimplyProjects\Config\Config;
+use CodingSimplyProjects\Model\Project;
 
 class ProjectsRepository {
 
+	/**
+	 * @return Project[]
+	 */
 	public static function find() {
-		$projects = get_posts( [ 'posts_per_page' => 5, 'post_type' =>  Config::PROJECT_POST_TYPE] );
+		$posts = get_posts( [ 'posts_per_page' => 5, 'post_type' =>  Config::PROJECT_POST_TYPE] );
+		$projects = [];
+		foreach ($posts as $post) {
+			$projects[] = Project::init($post);
+		}
 		return $projects;
 	}
 
--- src/Model/Project.php
+++ src/Model/Project.php
@@ -5,8 +5,187 @@ namespace CodingSimplyProjects\Model;
 
 use CodingSimplyProjects\Config\Config;
 
+/**
+ * Model that describes a project.
+ *
+ * @package CodingSimplyProjects\Model
+ */
 class Project {
 
+	/**
+	 * @var string
+	 */
+	public $git_url;
+
+	/**
+	 * @var string
+	 */
+	public $icon_url;
+
+	/**
+	 * @var string
+	 */
+	public $screenshot_url;
+
+	/**
+	 * @var string
+	 */
+	public $screenshot_url_2;
+
+	/**
+	 * @var string
+	 */
+	public $screenshot_url_3;
+
+	/**
+	 * @var string
+	 */
+	public $screenshot_url_4;
+
+	/**
+	 * @var string
+	 */
+	public $screenshot_url_5;
+
+	/**
+	 * @var \WP_Post
+	 */
+	protected $post;
+
+	/**
+	 * @var string
+	 */
+	protected $acfPrefix = 'coding_simply_project_';
+
+	/**
+	 * @var array
+	 */
+	protected $acfFields = [];
+
+	/**
+	 * Need to load the acf fields during construction of the project object.
+	 */
+	public function __construct() {
+		$this->acfFields = [
+			[
+				'key'           => 'field_598122f309509',
+				'label'         => 'Git URL',
+				'name'          => $this->acfPrefix . 'git_url',
+				'type'          => 'text',
+				'instructions'  => 'Publicly access url for you git repo. A README.md file should be accessible.',
+				'required'      => 1,
+				'default_value' => '',
+				'placeholder'   => 'http://',
+				'prepend'       => '',
+				'append'        => '',
+				'formatting'    => 'none',
+				'maxlength'     => '',
+			],
+			[
+				'key'           => 'field_5981253d9d8d5',
+				'label'         => 'Icon URL',
+				'name'          => $this->acfPrefix . 'icon_url',
+				'type'          => 'text',
+				'instructions'  => 'Publicly accessible url for project icon. Probably in your git repo.',
+				'required'      => 1,
+				'default_value' => '',
+				'placeholder'   => 'http://',
+				'prepend'       => '',
+				'append'        => '',
+				'formatting'    => 'none',
+				'maxlength'     => '',
+			],
+			[
+				'key'           => 'field_59822f1db0898',
+				'label'         => 'Screenshot URL',
+				'name'          => $this->acfPrefix . 'screenshot_url',
+				'type'          => 'text',
+				'instructions'  => 'Publicly accessible url for a project screenshot. Probably in your git repo.',
+				'default_value' => '',
+				'placeholder'   => 'http://',
+				'required'      => 1,
+				'prepend'       => '',
+				'append'        => '',
+				'formatting'    => 'none',
+				'maxlength'     => '',
+			],
+			[
+				'key'           => 'field_59822f3fb0899',
+				'label'         => 'Screenshot URL 2',
+				'name'          => $this->acfPrefix . 'screenshot_url_2',
+				'type'          => 'text',
+				'instructions'  => 'Publicly accessible url for a project screenshot. Probably in your git repo.',
+				'default_value' => '',
+				'placeholder'   => 'http://',
+				'prepend'       => '',
+				'append'        => '',
+				'formatting'    => 'none',
+				'maxlength'     => '',
+			],
+			[
+				'key'           => 'field_59822f58b089a',
+				'label'         => 'Screenshot URL 3',
+				'name'          => $this->acfPrefix . 'screenshot_url_3',
+				'type'          => 'text',
+				'instructions'  => 'Publicly accessible url for a project screenshot. Probably in your git repo.',
+				'default_value' => '',
+				'placeholder'   => 'http://',
+				'prepend'       => '',
+				'append'        => '',
+				'formatting'    => 'none',
+				'maxlength'     => '',
+			],
+			[
+				'key'           => 'field_59822f6ab089b',
+				'label'         => 'Screenshot URL 4',
+				'name'          => $this->acfPrefix . 'screenshot_url_4',
+				'type'          => 'text',
+				'instructions'  => 'Publicly accessible url for a project screenshot. Probably in your git repo.',
+				'default_value' => '',
+				'placeholder'   => 'http://',
+				'prepend'       => '',
+				'append'        => '',
+				'formatting'    => 'none',
+				'maxlength'     => '',
+			],
+			[
+				'key'           => 'field_59822f78b089c',
+				'label'         => 'Screenshot URL 5',
+				'name'          => $this->acfPrefix . 'screenshot_url_5',
+				'type'          => 'text',
+				'instructions'  => 'Publicly accessible url for a project screenshot. Probably in your git repo.',
+				'default_value' => '',
+				'placeholder'   => 'http://',
+				'prepend'       => '',
+				'append'        => '',
+				'formatting'    => 'none',
+				'maxlength'     => '',
+			]
+		];
+	}
+
+	/**
+	 * Get a Project from a Post.
+	 *
+	 * @param $post \WP_Post
+	 *
+	 * @return Project
+	 */
+	public static function init( $post ) {
+		$project = new Project();
+		$p       = get_fields( $post->ID );
+		foreach ( $project->acfFields as $key => $field ) {
+			$local             = str_replace( $project->acfPrefix, '', $field['name'] );
+			$project->{$local} = $p[ $field['name'] ];
+		}
+		$project->post = $post;
+
+		return $project;
+	}
+
+	/**
+	 * Creates the custom post type for the project.
+	 */
 	public function custom() {
 		$labels = [
 			'name'                  => _x( 'Projects', 'Project General Name', Config::TEXT_DOMAIN ),
@@ -61,107 +240,15 @@ class Project {
 		register_post_type( Config::PROJECT_POST_TYPE, $args );
 	}
 
+	/**
+	 * Creates a meta box for the project parameters on the custom post type using acf.
+	 */
 	public function meta_box() {
 		if ( function_exists( "register_field_group" ) ) {
 			register_field_group( [
-				'id'         => 'acf_coding_simply_project_info',
+				'id'         => 'acf_' . $this->acfPrefix . 'info',
 				'title'      => 'Project Info',
-				'fields'     => [
-					[
-						'key'           => 'field_598122f309509',
-						'label'         => 'Git URL',
-						'name'          => 'coding_simply_project_git_url',
-						'type'          => 'text',
-						'instructions'  => 'Publicly access url for you git repo. A README.md file should be accessible.',
-						'required'      => 1,
-						'default_value' => '',
-						'placeholder'   => 'http://',
-						'prepend'       => '',
-						'append'        => '',
-						'formatting'    => 'none',
-						'maxlength'     => '',
-					],
-					[
-						'key'           => 'field_5981253d9d8d5',
-						'label'         => 'Icon URL',
-						'name'          => 'coding_simply_project_icon_url',
-						'type'          => 'text',
-						'instructions'  => 'Publicly accessible url for project icon. Probably in your git repo.',
-						'required'      => 1,
-						'default_value' => '',
-						'placeholder'   => 'http://',
-						'prepend'       => '',
-						'append'        => '',
-						'formatting'    => 'none',
-						'maxlength'     => '',
-					],
-					[
-						'key'           => 'field_59822f1db0898',
-						'label'         => 'Screenshot URL',
-						'name'          => 'coding_simply_project_screenshot_url',
-						'type'          => 'text',
-						'instructions'  => 'Publicly accessible url for a project screenshot. Probably in your git repo.',
-						'default_value' => '',
-						'placeholder'   => 'http://',
-						'required'      => 1,
-						'prepend'       => '',
-						'append'        => '',
-						'formatting'    => 'none',
-						'maxlength'     => '',
-					],
-					[
-						'key'           => 'field_59822f3fb0899',
-						'label'         => 'Screenshot URL 2',
-						'name'          => 'coding_simply_project_screenshot_url_2',
-						'type'          => 'text',
-						'instructions'  => 'Publicly accessible url for a project screenshot. Probably in your git repo.',
-						'default_value' => '',
-						'placeholder'   => 'http://',
-						'prepend'       => '',
-						'append'        => '',
-						'formatting'    => 'none',
-						'maxlength'     => '',
-					],
-					[
-						'key'           => 'field_59822f58b089a',
-						'label'         => 'Screenshot URL 3',
-						'name'          => 'coding_simply_project_screenshot_url_3',
-						'type'          => 'text',
-						'instructions'  => 'Publicly accessible url for a project screenshot. Probably in your git repo.',
-						'default_value' => '',
-						'placeholder'   => 'http://',
-						'prepend'       => '',
-						'append'        => '',
-						'formatting'    => 'none',
-						'maxlength'     => '',
-					],
-					[
-						'key'           => 'field_59822f6ab089b',
-						'label'         => 'Screenshot URL 4',
-						'name'          => 'coding_simply_project_screenshot_url_4',
-						'type'          => 'text',
-						'instructions'  => 'Publicly accessible url for a project screenshot. Probably in your git repo.',
-						'default_value' => '',
-						'placeholder'   => 'http://',
-						'prepend'       => '',
-						'append'        => '',
-						'formatting'    => 'none',
-						'maxlength'     => '',
-					],
-					[
-						'key'           => 'field_59822f78b089c',
-						'label'         => 'Screenshot URL 5',
-						'name'          => 'coding_simply_project_screenshot_url_5',
-						'type'          => 'text',
-						'instructions'  => 'Publicly accessible url for a project screenshot. Probably in your git repo.',
-						'default_value' => '',
-						'placeholder'   => 'http://',
-						'prepend'       => '',
-						'append'        => '',
-						'formatting'    => 'none',
-						'maxlength'     => '',
-					],
-				],
+				'fields'     => $this->acfFields,
 				'location'   => [
 					[
 						[
@@ -178,7 +265,7 @@ class Project {
 					'layout'         => 'default',
 					'hide_on_screen' => [],
 				],
-				'menu_order' => 0,
+				'menu_order' => 0
 			] );
 		}
 	}
